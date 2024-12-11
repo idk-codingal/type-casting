d93bb5ebf7bfc236ed0f96a93d2b6e0d2c07b06a
idk-codingal <mokshnagda2020@gmail.com>
2024-12-10 17:12:45 -0800

    first commit

---
+++ main2.py
@@ -0,0 +1,12 @@
+n = "Moksh"
+h = 10
+is_student = "True"
+kg = 42.2
+print(n)
+print(h)
+print(is_student)
+print(kg)
+print("n :", n)
+print("h :", h)
+print("is_student :", is_student)
+print("kg :", kg)
